2a8bd7ec40b9eaea9ba617c03341f667c224b411
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-06-23 20:19:45 -0300

    Tweetero - primeiros bonuz

--- src/app.js
+++ src/app.js
@@ -6,37 +6,51 @@ app.use(cors())
 app.use(express.json());
 const PORT = 5000
 const user = {
-	username: "", 
-	avatar: "" 
+	username: "",
+	avatar: ""
 }
 const tweets = []
 app.post("/sign-up", (req, res) => {
-	const {username, avatar} = req.body
-	user.username = username
-	user.avatar = avatar
-	res.send(user)
+	const { username, avatar } = req.body
+	
 
+	// && typeof(username, avatar)  === String
+	// && typeof(username, avatar)  === String 
+
+	if (username === '' || typeof(username) !== 'string'|| 
+	avatar === '' || typeof(avatar) !== 'string') {
+		res.status(400).send('Todos os campos são obrigatórios!')
+	}
+	else {
+		user.username = username
+		user.avatar = avatar
+		res.send('OK')
+	}
 })
 
 
 app.post("/tweets", (req, res) => {
-    const {username, tweet} = req.body
-	const newTweet = {
-		username: username,
-		avatar: user.avatar,
-		tweet: tweet
+	const { username, tweet } = req.body
+	if (username === '' || tweet === '') {
+		res.status(400).send('UNAUTHORIZED')
+	} else {
+		const newTweet = {
+			username: username,
+			avatar: user.avatar,
+			tweet: tweet
+		}
+		tweets.push(newTweet)
+
+		res.send('Tweet feito com sucesso');
 	}
-	tweets.push(newTweet)
-	
-    res.send('Tweet feito com sucesso');
 });
 
 app.get("/tweets", (req, res) => {
 	//.slice(-2,-10)
-   
-	if(tweets.length > 10){
-		 res.send(tweets.slice(-10));
-	}else{
+
+	if (tweets.length > 10) {
+		res.send(tweets.slice(-10));
+	} else {
 		res.send(tweets);
 	}
 });
